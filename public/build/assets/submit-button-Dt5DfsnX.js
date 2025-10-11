cbb120d2278ff269e5abb783a06f76bdb3712424
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-10-11 13:53:50 +0800

    update build

--- public/build/assets/submit-button-Dt5DfsnX.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-D0ySo36_.js";import{B as o}from"./createLucideIcon-BHnGhaSz.js";import{L as e}from"./loader-circle-e-0Qb-qY.js";import{C as a}from"./check-RnYQuMbD.js";const j=({label:m,icon:t,loading:r,...i})=>s.jsxs(o,{type:"submit",disabled:r,...i,children:[r?s.jsx(e,{className:"animate-spin"}):t?s.jsx(t,{}):s.jsx(a,{}),m??"Submit"]});export{j as S};
